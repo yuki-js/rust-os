f26c186f576c8d68a1c450d19dce31bfbccb5ea1
yuki-js <20838151+yuki-js@users.noreply.github.com>
2020-04-09 21:26:57 +0900

    draw call

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,11 @@
 #![feature(start)]
 
 use core::panic::PanicInfo;
+mod utils;
 
 #[no_mangle]
-fn hlt() {
-    unsafe {
-        // assembly で "HLT" したのと同じ効果がある。
-        asm!("hlt");
-    }
-}
-
-#[no_mangle]
-fn show_gray(i: u32) {
-    let a: u8 = 8;
+fn draw(i: u32) {
+    let a: u8 = i as u8 & 0x0f;
     let ptr = unsafe { &mut *(i as *mut u8) };
     *ptr = a 
 }
@@ -24,16 +17,16 @@ fn show_gray(i: u32) {
 pub extern "C" fn haribote_os() -> ! {
     // 本にある通り、0xa0000から0xaffffまで描画
     for i in 0xa0000..0xaffff {
-      show_gray(i);
+      draw(i);
     }
     loop {
-        hlt()
+        utils::hlt();
     }
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {
-        hlt()
+        utils::hlt()
     }
 }
---
+++ src/utils.rs
@@ -0,0 +1,5 @@
+pub fn hlt() {
+    unsafe {
+        asm!("hlt");
+    }
+}
